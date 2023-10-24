7888c138d8fea200685d0d6a51c0051a1c42a3ae
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-10-24 16:37:56 +0300

    tabulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app1';
+export class AppComponent implements OnInit {
+  title :string = '';
+  color1 :string = '';
+
+  constructor(){}
+  ngOnInit(): void {
+    this.title = "app1";
+    this.color1 = "yellow";
+  }
+
+  getColor(): string{
+    return this.color1;
+  }
 }
+
+
