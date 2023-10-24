aec2071ac0929bdca1e850a921ed995cb9f391d1
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-10-24 18:46:08 +0300

    second lesson angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from './models/book.model';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,44 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title :string = '';
   color1 :string = '';
+  message :string = 'Hello';
+  image_url :string = './favicon.ico';
+  books:Book[] = [];
+  book:Book | undefined
 
   constructor(){}
   ngOnInit(): void {
     this.title = "app1";
-    this.color1 = "yellow";
+    this.color1 = "blue";
+
+    this.books = [
+    new Book(1, "Goblin Reservetion", "Clifford Simak", 250),
+    new Book(2, "I, robot", "Isaac Asimov", 220),
+    new Book(3, "Solaris", "Stanislav Lem", 300)
+    ];
+    
   }
 
   getColor(): string{
-    return this.color1;
+    if(this.message.length < 10) {
+      return this.color1;
+    }
+    else {
+      return "green";
+    }
+  }
+  
+  changeMessage(elem: any): void {
+    this.message = elem.target.value;
+  }
+
+  getColorByName(color:string): string {
+    this.color1 = color
+    return this.color1
+  }
+
+  checkCondition(): boolean{
+    return this.color1=="violet";
   }
 }
 
